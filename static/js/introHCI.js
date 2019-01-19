5d09e7478381bf858f8fff8830be643c998c4534
Stig Terrebonne <stigterrebonne@gmail.com>
2019-01-18 16:08:26 -0800

    update js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -24,11 +24,10 @@ function projectClick(e) {
 	e.preventDefault();
 	$(this).css("background-color", "#7fff00");
 	var containingProject = $(this).closest(".project");
-	$(".project p").toggle();
     var description = $(containingProject).find(".project-description");
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       $(".project p").toggle();
     }
 }
